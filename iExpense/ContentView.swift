bee0e8b9175393e86321b7a4b5c454c1d887b743
Simon Chao <ysc406@nyu.edu>
2021-07-27 10:30:14 -0400

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by YunShou Chao on 7/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by YunShou Chao on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
