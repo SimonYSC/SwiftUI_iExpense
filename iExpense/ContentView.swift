86de100f36d9ab29050ea6e898a0acade8f2d914
Simon Chao <ysc406@nyu.edu>
2021-07-27 11:36:47 -0400

    Create iExpense project.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,62 @@
 
 import SwiftUI
 
+//class User: ObservableObject {
+//    @Published var firstName = "Bilbo"
+//    @Published var lastName = "Baggins"
+//}
+
+//struct SecondView: View {
+//    @Environment(\.presentationMode) var presentationMode
+//    var name: String
+//
+//    var body: some View {
+//        Button("Dismiss") {
+//            self.presentationMode.wrappedValue.dismiss()
+//        }
+//    }
+//}
+
 struct ContentView: View {
+//    @ObservedObject private var user = User()
+//    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List {
+                    // onDelete only works on ForEach
+                    ForEach(numbers, id: \.self) {
+                        Text("\($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                }
+                .navigationBarItems(leading: EditButton())
+            }
+        }
+        
+//        Button("Show Sheet") {
+//            self.showingSheet.toggle()
+//        }
+//        .sheet(isPresented: $showingSheet) {
+//            SecondView(name: "Frodo")
+//        }
+//        VStack {
+//            Text("Your name is \(user.firstName) \(user.lastName)")
+//            TextField("First name", text: $user.firstName)
+//            TextField("Last name", text: $user.lastName)
+//        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
